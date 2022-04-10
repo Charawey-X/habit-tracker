88c35370a1dadba5f2f359f23abefb387ddc03dc
Charawey-X <charawey@gmail.com>
2022-04-10 21:18:38 +0300

    upto editing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Habit } from './models/habit';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'habit-tracker';
+  public adding = false;
+
+  public editing = false;
+  public editingIndex!: number;
+
+  public habitForm = new FormGroup({
+    name: new FormControl(''),
+    frequency: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  public onSubmit() {
+    const habit = this.habitForm.value as Habit;
+
+    if (this.editing) {
+      this.habits.splice(this.editingIndex, 1, habit);
+    } else {
+      this.habits.push(habit);
+    }
+
+    this.editing = false;
+    this.adding = false;
+  }
+
+  public habits: Habit[] = [
+    <Habit>{
+      name: '15 Minute Walk',
+      frequency: 'Daily',
+      description:
+        'This habit makes my kitchen look nice and makes my day better the next morning.',
+    },
+    <Habit>{
+      name: 'Weed the Garden',
+      frequency: 'Weekly',
+      description:
+        'The weeds get so out of hand if they wait any longer, and I like how nice our home looks with a clean lawn.',
+    },
+  ];
+
+  public setEditForm(habit: Habit, index: number) {
+    this.habitForm.patchValue({
+      name: habit.name,
+      frequency: habit.frequency,
+      description: habit.description,
+    });
+    this.editing = true;
+    this.editingIndex = index;
+  }
 }
+
